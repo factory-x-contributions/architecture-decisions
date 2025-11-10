6975e22e063f0cfce1cbf92bdee91d8067eb8bc4
JanisKretschmann <jnskret@gmail.com>
2025-11-10 13:23:52 +0100

    docusaurus static website components added

---
+++ docusaurus.config.js
@@ -0,0 +1,122 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// README einfügen auf Toplevel
+// ADRs sollen Toplevel -> nicht als Unterordner
+// Ideal : Ein Folder mit Website, Ein Folder für Hercules usw. 
+// https://github.com/eclipse-tractusx/tractusx-edc/blob/main/.github/dependabot.yml
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Factory-X Contributions',
+  tagline: 'We are creating an open and collaborative digital ecosystem for factory outfitters and operators, built on the foundations of Catena‑X and the principles of Plattform Industrie 4.0.',
+  favicon: 'https://factory-x.org/wp-content/uploads//factory-x-logo.svg',
+  baseUrl: '/',
+  url: 'https://factory-x-contributions.github.io',
+  onBrokenLinks: 'throw',
+  onBrokenAnchors: 'throw',
+  future: {
+    v4: true,
+  },
+
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'https://factory-x.org/wp-content/uploads//factory-x-logo.svg',
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: 'Factory-X Contributions',
+        logo: {
+          alt: 'Factory-X Logo',
+          src: 'https://factory-x.org/wp-content/uploads//factory-x-logo.svg',
+        },
+        items: [
+          {to: 'architecture-decisions/fx_docs/hercules/adr002-authorization-discovery/', label: 'Hercules', position: 'left'},
+          {to: './docs/leo/leo_placeholder', label: 'Leo', position: 'left'},
+          {to: './fx_docs/orion/orion_placeholder', label: 'Orion', position: 'left'},
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/factory-x-contributions',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Hercules',
+                to: '../fx_docs/hercules/adr002-authorization-discovery/',
+              },
+            ],
+          },
+          {
+            title: 'General Information',
+            items: [
+              {
+                label: 'Legal Notice',
+                href: 'https://factory-x.org/de/impressum/',
+              },
+              {
+                label: 'Privacy Policy',
+                href: 'https://factory-x.org/privacy-policy/',
+              },
+              {
+                label: 'Contact',
+                href: 'https://factory-x.org/contact/',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/factory-x-contributions',
+              },
+              {
+                label: 'Website',
+                href: 'https://factory-x.org/de/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Factory-X, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
